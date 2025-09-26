3f0eb0a35bb76c986d5500a35438c97aa3543b52
Krishna Kumar <kkdakshana@gmail.com>
2025-09-26 10:47:38 +0530

    create storage account

---
+++ storage-account/variables.tf
@@ -0,0 +1,12 @@
+variable "storage_account_name" {
+  type        = string
+  description = "The name of storage account"
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
